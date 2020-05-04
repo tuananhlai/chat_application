48e2b01c74564b0ed12195eab26502f3874ea297
anhkk1245 <57492209+anhkk1245@users.noreply.github.com>
2020-05-04 11:12:06 +0700

    slide in and out for workspace 2

--- client/src/components/home/Workspace.vue
+++ client/src/components/home/Workspace.vue
@@ -6,16 +6,29 @@
       @toggleChannelDetails="toggleChannelDetails"
     />
     
+    <transition
+      name="expand"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @leave="leave">
+
     <workspace-secondary-channel-details
       v-show="show === 'channelDetails'"
       @close="closeSecondaryWorkspace"
     />
-    
+    </transition>
+
+    <transition
+      name="expand"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @leave="leave">
     <workspace-secondary-replies
       :thread-master="threadMaster"
       v-show="show === 'reply'"
       @close="closeSecondaryWorkspace"
     />
+    </transition>
     
   </div>
 </template>
@@ -49,6 +62,28 @@ export default {
     },
     closeSecondaryWorkspace() {
       this.show = "";
+    },
+    enter(el) {
+      el.style.width = 0;
+      
+      getComputedStyle(el);
+
+      setTimeout(() => {
+        el.style.width = '300px';
+      });
+      
+    },
+    afterEnter(el) {
+      el.style.width = '300px';
+    },
+    leave(el) {
+      el.style.width = getComputedStyle(el).width;
+
+      getComputedStyle(el);
+
+      setTimeout(() => {
+        el.style.width = 0;
+      });
     }
   }
 };
@@ -62,5 +97,15 @@ export default {
   display: flex;
   flex-direction: row;
 }
+
+.expand-enter-active {
+  transition: width .5s ease-in-out;
+}
+
+.expand-leave-active {
+  transition: width .5s ease-in-out;
+  overflow: hidden;
+}
+
 
 </style>
